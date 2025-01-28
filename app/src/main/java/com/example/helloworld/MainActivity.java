1d0acedd6d923d862dd32df7c9cea04ae519fbba
yohei.hokari <yohei.hokari@gmail.com>
2025-01-28 22:39:41 +0900

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.text1);
+                textView.setText(R.string.second_push_btn);
+            }
+        });
+    }
+
+    public void onClickBth(View view) {
+        TextView textView = (TextView) findViewById(R.id.text1);
+                textView.setText(R.string.push_btn);
+    }
+}
